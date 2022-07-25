0fb9e29ab332b66f720e268e4054688c67e06f8f
Teejay Dixon <tahjgdixon@gmail.com>
2022-07-25 00:59:13 -0700

    commit tree 14ae5f33989d5aff081a36feca3ed7d025420020

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+function introduction(name) {
+
+  return `Hi, my name is ${name}.`;
+}
+
+console.log(introduction("Teejay"));
+
+
+
+
+
+function introductionWithLanguage(name, language) {
+
+  return "Hi, my name is Teejay and I am learning to program in Javascript.");
+
+}
+
+
+console.log(introductionWithLanguage());
